d125b9dfac801dc70220e04fb00ff4caf12f310a
csutar911-source <csutar911@gmail.com>
2025-10-23 16:23:36 +0530

    added 2 files
    added helloworld.c , intro.c

---
+++ LAB_01_Task/intro.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main (){
+    printf("Chetan Sutar\n");
+    printf("Roll No. : MEB25028\n");
+    printf("Department : Mechanical Engineering\n");
+    printf("Tezpur University\n");
+    return 0;
+}
