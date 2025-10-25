74eef80097adbb3bfe9d16bbdbb4d439ed3a9e36
csutar911-source <csutar911@gmail.com>
2025-10-25 12:49:23 +0530

    Fix kinetic energy calculation formula
    fix it i accidently upload wrong file

--- LAB_02_Task/Kinetic_Energy_Calculator.c
+++ LAB_02_Task/Kinetic_Energy_Calculator.c
@@ -10,7 +10,8 @@ int main(){
     printf("Enter Velocity =");
     scanf("%f" , &Velocity);
 
-    printf("Kinetic_Energy is :%f" , 0.5 * Mass * Velocity ); 
+    printf("Kinetic_Energy is :%f" , 0.5 * Mass * Velocity * Velocity ); 
 
     return 0;
 }
+
