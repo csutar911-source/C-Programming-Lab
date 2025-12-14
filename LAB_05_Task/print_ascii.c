d3bc13ee1f54fbbf45f71f3d5334ea77a0496149
csutar911-source <csutar911@gmail.com>
2025-12-14 18:42:36 +0530

    added 2 files
    added ascii_check.c and print_ascii.c

---
+++ LAB_05_Task/print_ascii.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    char letter;
+
+    // ASCII - American Standard Code for Information Interchange
+
+    printf("Enter a character or number: ");
+    scanf("%c", &letter);
+
+    printf("The ASCII value of the given input is %d\n", letter);
+
+    return 0;
+}
